d3b8a21ec016ac7efc33aa9f01fc5271971330b4
adamgfraser <adam.fraser@gmail.com>
2016-01-07 16:35:40 -0800

    implement Yelp client

---
+++ lib/yelp_client.rb
@@ -0,0 +1,24 @@
+require 'nokogiri'
+require 'yelp'
+
+class YelpClient
+
+  attr_reader :client
+
+  def initialize
+    html = File.read("fixtures/api_keys.html")
+    doc = Nokogiri::HTML(html)
+    @client = Yelp::Client.new
+    client.configure do |config|
+      config.consumer_key = doc.css("td")[0].text
+      config.consumer_secret = doc.css("td")[1].text
+      config.token = doc.css("td")[2].text
+      config.token_secret = doc.css("td")[3].text
+    end
+  end
+
+  def search(location, params = {}, locale = {})
+    client.search(location, params, locale)
+  end
+
+end
